0032f0eba53298e55a1a1bad04cb811e630df103
alcivarv <52640517+alcivarv@users.noreply.github.com>
2022-02-25 23:50:15 -0500

    Update App.js
    added the gas limit note

--- src/App.js
+++ src/App.js
@@ -386,8 +386,8 @@ function App() {
               color: "var(--primary-text)",
             }}
           >
-            Please make sure you are connected to the right network (
-            {CONFIG.NETWORK.NAME} Mainnet) and the correct address. <br/> Please note:
+           <br/>Please make sure you are connected to the right network (
+            {CONFIG.NETWORK.NAME} Mainnet) and the correct address.<br/> Please note:
             Once you make the purchase, you cannot undo this action.
           </s.TextDescription>
           <s.SpacerSmall />
@@ -397,11 +397,13 @@ function App() {
               color: "var(--primary-text)",
             }}
           >
+            We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
+            successfully mint your NFT.<br/> We recommend that you don't lower the
+            gas limit.
           </s.TextDescription>
         </s.Container>
       </s.Container>
     </s.Screen>
   );
 }
-
 export default App;
